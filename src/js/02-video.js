be15b140b50f51b728f90943eca2080b5ffe85a0
IYuliia <catnew2012@gmail.com>
2023-11-05 19:32:50 +0100

    added videoplayer current time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+player.on('loaded', function () {
+  const savedTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+
+  if (!isNaN(savedTime)) {
+    player.setCurrentTime(savedTime);
+  }
+});
